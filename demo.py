34fb177ebd0dd4aefb2f5cb877219c4d421cb925
sawan16 <sawanrai777@gmail.com>
2019-02-22 09:36:37 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,40 @@
+
+# This example show the use of p2x(Updated Lark) tool for python to XML conversion.
+
+
+import sys
+import os, os.path
+from io import open
+import glob, time
+
+from lark import Lark
+from lark.indenter import Indenter
+
+class PythonIndenter(Indenter):
+    NL_type = '_NEWLINE'
+    OPEN_PAREN_types = ['LPAR', 'LSQB', 'LBRACE']
+    CLOSE_PAREN_types = ['RPAR', 'RSQB', 'RBRACE']
+    INDENT_type = '_INDENT'
+    DEDENT_type = '_DEDENT'
+    tab_len = 8
+
+def _read(fn, *args):
+    kwargs = {'encoding': 'iso-8859-1'}
+    with open(fn, *args, **kwargs) as f:
+        return f.read()
+
+if len(sys.argv)<3:
+	print("Please Provide source and target file name correctly....")
+	exit()
+
+kwargs = dict(rel_to=__file__, postlex=PythonIndenter(), start='file_input')
+
+
+python_parser3 = Lark.open('python3.lark',parser='lalr', **kwargs)
+
+tree = python_parser3.parse(_read(sys.argv[1]) + '\n')
+
+fl = open(sys.argv[2],'w')
+fl.write(str(tree))
+fl.close()
+print(tree)
